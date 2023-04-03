5d79ca50afa33e3abefe3d472558d989227c8664
nuhmisirli <misir002@umn.edu>
2023-04-03 10:12:41 -0500

    second_activity.xml loaded

--- app/src/main/java/com/example/myapplicationnavigation/MainActivity.java
+++ app/src/main/java/com/example/myapplicationnavigation/MainActivity.java
@@ -3,15 +3,7 @@ package com.example.myapplicationnavigation;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
@@ -34,4 +26,18 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra(EXTRA_MESSAGE, message);
             startActivity(intent);
         }
+
+    public static class SecondActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.secondactivity);
+
+            Intent intent=getIntent();
+            String message = intent.getStringExtra(EXTRA_MESSAGE);
+    //
+    //        TextView textView=(TextView)findViewById(R.id.GetStarted);
+    //        textView.setText(message);
+        }
+    }
 }
